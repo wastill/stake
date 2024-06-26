90937cad92b9d4b15a275d3e6d132ec60110bdae
wastill <2225144254@qq.com>
2024-06-26 18:35:28 +0800

    init

---
+++ contracts/Pool.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Pool {
+//     stTokenAddress: 质押代币的地址。
+// poolWeight: 质押池的权重，影响奖励分配。
+// lastRewardBlock: 最后一次计算奖励的区块号。
+// accB2PerST: 每个质押代币累积的 B2 数量。
+// stTokenAmount: 池中的总质押代币量。
+// minDepositAmount: 最小质押金额。
+// unstakeLockedBlocks: 解除质押的锁定区块数。
+}
+
